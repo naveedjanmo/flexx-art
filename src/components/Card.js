080c4a34548c5ee75934efe2adf2495cfb995319
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2022-02-23 08:44:22 +0000

    bug fix attempt

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Check UD videos for if statements about meta data availablity / if statements used
 // in src tag. Problem: some NFTs don't have the URL tag.
 
 const Card = ({ nft }) => {
+  // const urlCheck = () => {
+  //   if (nft.meta.content[1] !== 'undefined') {
+  //     return nft.meta.content[1].url;
+  //   } else {
+  //     return null;
+  //   }
+  // };
+
+  const [image, setImage] = useState('');
+
+  const getImageUrl = async () => {
+    if (nft.meta.content[1] !== 'undefined') {
+      const url = await nft.meta.content[1].url();
+    }
+
+    setImage(url);
+  };
+
+  console.log(nft.meta.content[1].url);
+
   return (
     <div className="card-wrap">
-      <img src={() => nft.meta.content[0].url} alt="" />
+      <img src={nft.meta.content[1]} alt="" />
       <div className="card-content-wrap">
         <div className="card-content">
           <p className="card-collection-title">Collection Name</p>
